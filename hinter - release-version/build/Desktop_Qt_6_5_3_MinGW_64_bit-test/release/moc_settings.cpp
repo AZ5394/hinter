098f9c9aba761a4e70c93e211bc80565bb7a722e
AZ5394 <103622688+AZ5394@users.noreply.github.com>
2024-11-27 22:09:32 +0800

    Add files via upload

---
+++ hinter - release-version/build/Desktop_Qt_6_5_3_MinGW_64_bit-test/release/moc_settings.cpp
@@ -0,0 +1,283 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'settings.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../settings.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'settings.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSsettingsENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSsettingsENDCLASS = QtMocHelpers::stringData(
+    "settings",
+    "tableNameChanged",
+    "",
+    "tableName",
+    "on_lineEdit_returnPressed",
+    "on_createTable_clicked",
+    "on_comboBox_currentIndexChanged",
+    "index",
+    "on_deleteTable_clicked",
+    "on_clearTable_clicked",
+    "handleDeleteButtonClicked",
+    "onIsShowButtonClicked",
+    "row",
+    "QPushButton*",
+    "button",
+    "on_refreshButton_clicked",
+    "reloadIsShowButton"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSsettingsENDCLASS_t {
+    uint offsetsAndSizes[34];
+    char stringdata0[9];
+    char stringdata1[17];
+    char stringdata2[1];
+    char stringdata3[10];
+    char stringdata4[26];
+    char stringdata5[23];
+    char stringdata6[32];
+    char stringdata7[6];
+    char stringdata8[23];
+    char stringdata9[22];
+    char stringdata10[26];
+    char stringdata11[22];
+    char stringdata12[4];
+    char stringdata13[13];
+    char stringdata14[7];
+    char stringdata15[25];
+    char stringdata16[19];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSsettingsENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSsettingsENDCLASS_t qt_meta_stringdata_CLASSsettingsENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 8),  // "settings"
+        QT_MOC_LITERAL(9, 16),  // "tableNameChanged"
+        QT_MOC_LITERAL(26, 0),  // ""
+        QT_MOC_LITERAL(27, 9),  // "tableName"
+        QT_MOC_LITERAL(37, 25),  // "on_lineEdit_returnPressed"
+        QT_MOC_LITERAL(63, 22),  // "on_createTable_clicked"
+        QT_MOC_LITERAL(86, 31),  // "on_comboBox_currentIndexChanged"
+        QT_MOC_LITERAL(118, 5),  // "index"
+        QT_MOC_LITERAL(124, 22),  // "on_deleteTable_clicked"
+        QT_MOC_LITERAL(147, 21),  // "on_clearTable_clicked"
+        QT_MOC_LITERAL(169, 25),  // "handleDeleteButtonClicked"
+        QT_MOC_LITERAL(195, 21),  // "onIsShowButtonClicked"
+        QT_MOC_LITERAL(217, 3),  // "row"
+        QT_MOC_LITERAL(221, 12),  // "QPushButton*"
+        QT_MOC_LITERAL(234, 6),  // "button"
+        QT_MOC_LITERAL(241, 24),  // "on_refreshButton_clicked"
+        QT_MOC_LITERAL(266, 18)   // "reloadIsShowButton"
+    },
+    "settings",
+    "tableNameChanged",
+    "",
+    "tableName",
+    "on_lineEdit_returnPressed",
+    "on_createTable_clicked",
+    "on_comboBox_currentIndexChanged",
+    "index",
+    "on_deleteTable_clicked",
+    "on_clearTable_clicked",
+    "handleDeleteButtonClicked",
+    "onIsShowButtonClicked",
+    "row",
+    "QPushButton*",
+    "button",
+    "on_refreshButton_clicked",
+    "reloadIsShowButton"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSsettingsENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,   74,    2, 0x06,    1 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       4,    0,   77,    2, 0x08,    3 /* Private */,
+       5,    0,   78,    2, 0x08,    4 /* Private */,
+       6,    1,   79,    2, 0x08,    5 /* Private */,
+       8,    0,   82,    2, 0x08,    7 /* Private */,
+       9,    0,   83,    2, 0x08,    8 /* Private */,
+      10,    0,   84,    2, 0x08,    9 /* Private */,
+      11,    2,   85,    2, 0x08,   10 /* Private */,
+      15,    0,   90,    2, 0x08,   13 /* Private */,
+      16,    1,   91,    2, 0x0a,   14 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, 0x80000000 | 13,   12,   14,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   12,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject settings::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_CLASSsettingsENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSsettingsENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSsettingsENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<settings, std::true_type>,
+        // method 'tableNameChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        // method 'on_lineEdit_returnPressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_createTable_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_comboBox_currentIndexChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_deleteTable_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_clearTable_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'handleDeleteButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onIsShowButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QPushButton *, std::false_type>,
+        // method 'on_refreshButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'reloadIsShowButton'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const int &, std::false_type>
+    >,
+    nullptr
+} };
+
+void settings::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<settings *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->tableNameChanged((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 1: _t->on_lineEdit_returnPressed(); break;
+        case 2: _t->on_createTable_clicked(); break;
+        case 3: _t->on_comboBox_currentIndexChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 4: _t->on_deleteTable_clicked(); break;
+        case 5: _t->on_clearTable_clicked(); break;
+        case 6: _t->handleDeleteButtonClicked(); break;
+        case 7: _t->onIsShowButtonClicked((*reinterpret_cast< std::add_pointer_t<int>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QPushButton*>>(_a[2]))); break;
+        case 8: _t->on_refreshButton_clicked(); break;
+        case 9: _t->reloadIsShowButton((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 7:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 1:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QPushButton* >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (settings::*)(const QString & );
+            if (_t _q_method = &settings::tableNameChanged; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject *settings::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *settings::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSsettingsENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int settings::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void settings::tableNameChanged(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
